ed04522cdebedec3763cfdf61eb2571f8cb8159d
BackSH00TER <18689469+BackSH00TER@users.noreply.github.com>
2023-11-04 20:17:52 -0700

    remove manifest changes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,25 +9,16 @@ shared_script {
     -- '@ox_lib/init.lua', -- Uncomment this if you are planning to use any ox scripts (such as ox notify)
     'shared/config.lua',
     'shared/framework.lua',
-    'addons/printers/config.lua',
 }
 
 client_scripts {
     'client/client.lua',
     'client/pushables.lua',
     'client/placeables.lua',
-    'addons/printers/client.lua',
 }
 
 server_scripts {
     'server/server.lua',
-    'addons/printers/server.lua',
-}
-
-ui_page "addons/printers/html/index.html"
-
-files {
-    "addons/printers/html/*",
 }
 
 lua54 'yes'
