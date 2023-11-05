a6bf2187a4aee786964e5729a98efe847f96c455
BackSH00TER <18689469+BackSH00TER@users.noreply.github.com>
2023-11-04 18:41:57 -0700

    remove yogamat files (moved to separate repo)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,7 +9,6 @@ shared_script {
     -- '@ox_lib/init.lua', -- Uncomment this if you are planning to use any ox scripts (such as ox notify)
     'shared/config.lua',
     'shared/framework.lua',
-    'addons/yogamats/config.lua',
     'addons/printers/config.lua',
 }
 
@@ -17,7 +16,6 @@ client_scripts {
     'client/client.lua',
     'client/pushables.lua',
     'client/placeables.lua',
-    'addons/yogamats/yogamats.lua',
     'addons/printers/client.lua',
 }
 
