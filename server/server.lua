5728a6fcaf7515276d7bda52a174808fba86eaa9
ohscxr <scarishawt@gmail.com>
2025-01-01 00:33:13 -0500

    spawn item exploit fix

--- server/server.lua
+++ server/server.lua
@@ -5,6 +5,16 @@ for _, prop in pairs(Config.PlaceableProps) do
     end)
 end
 
+-- Checks the config to see if the item is valid
+local function containsItem(item)
+    for i = 1, #Config.PlaceableProps do
+        local v = Config.PlaceableProps[i]
+        if v.item == item then
+            return true
+        end
+    end
+end
+
 -- This function is to handle the syncing of deleting world props between all clients
 RegisterServerEvent("wp-placeables:server:deleteWorldObject", function(object)
     TriggerClientEvent("wp-placeables:client:deleteWorldObject", -1, object)
@@ -17,5 +27,11 @@ end)
 
 RegisterNetEvent("wp-placeables:server:AddItem", function(itemName)
     local src = source
+
+    if not containsItem(itemName) then
+        print(string.format("%s - tried to spawn an item that does not exist in the config (%s)", src, itemName))
+        return
+    end
+
     AddItem(src, itemName, 1)
 end)
